9d984c018abe25d6575dcc58de61d6374a8b8167
Alexey Zamnius <a.zamnius@me.com>
2026-06-22 09:30:25 +0300

    window_extraction.py: allowed for skipping target

--- scripts/window_extraction.py
+++ scripts/window_extraction.py
@@ -1,4 +1,4 @@
-def window_extraction(con, dates: dict):
+def window_extraction(con, dates: dict, target: bool = True):
 
     # Выделение feature-window
     df_feature = con.execute(f"""
@@ -10,15 +10,20 @@ def window_extraction(con, dates: dict):
     """).df()
 
     # Выделение target-window
-    df_target = con.execute(f"""
-        SELECT *
-        FROM transactions
-        WHERE t_dat BETWEEN 
-            '{dates['target_window_start']}' AND 
-            '{dates['target_window_end']}'
-    """).df()
+    if target:
+        df_target = con.execute(f"""
+            SELECT *
+            FROM transactions
+            WHERE t_dat BETWEEN 
+                '{dates['target_window_start']}' AND 
+                '{dates['target_window_end']}'
+        """).df()
+    else:
+        df_target = None
 
+    # Логи
     print(f'Кол-во наблюдений на feature_window: {len(df_feature)}')
-    print(f'Кол-во наблюдений на target_window: {len(df_target)}')
+    if target:
+        print(f'Кол-во наблюдений на target_window: {len(df_target)}')
 
     return df_feature, df_target
