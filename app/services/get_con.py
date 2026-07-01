91f95f1a8247aee2e196bf31be7ec8ca8573d8e7
Alexey Zamnius <a.zamnius@me.com>
2026-07-01 17:14:53 +0300

    add pathlib to FastApi app

--- app/services/get_con.py
+++ app/services/get_con.py
@@ -1,5 +1,7 @@
 import duckdb
 
+from scripts.paths import DATA_RAW_DIR, DATA_RECOMMENDATIONS_DIR
+
 def get_con():
 
     # Предопределяем БД
@@ -7,9 +9,9 @@ def get_con():
 
     # Словарь с названиями таблиц и путями к данным
     datasets = {
-        'recommendations': 'data/recommendations/df_rec.parquet',
-        'articles': 'data/raw/articles.parquet',
-        'customers': 'data/raw/customers.parquet'
+        'recommendations': DATA_RECOMMENDATIONS_DIR / 'df_rec.parquet',
+        'articles': DATA_RAW_DIR / 'articles.parquet',
+        'customers': DATA_RAW_DIR / 'customers.parquet'
     }
     
     # Загружаем данные в БД
@@ -18,7 +20,7 @@ def get_con():
         con.execute(f"""
             CREATE OR REPLACE TABLE {name} AS
             SELECT *
-            FROM read_parquet('{path}')
+            FROM read_parquet('{path.as_posix()}')
         """)
 
     return con
