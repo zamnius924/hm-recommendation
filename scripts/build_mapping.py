66a58b2bccbb9963c6d60460b6c563cd0a4213b5
Alexey Zamnius <a.zamnius@me.com>
2026-07-06 18:29:02 +0300

    fix indexes in build_mapping

--- scripts/build_mapping.py
+++ scripts/build_mapping.py
@@ -21,21 +21,21 @@ def build_mapping(*dfs):
 
     # Словари для отображения индетификаторов в индексы
     customer_id2index = {
-        customer_id: i 
+        customer_id: (i + 1) 
         for i, customer_id in enumerate(all_customers)
     }
     article_id2index = {
-        article_id: i 
+        article_id: (i + 1)
         for i, article_id in enumerate(all_articles)
     }
 
     # Словари для отображения индексов в идентификаторы (обратные словари)
     customer_index2id = {
-        i: customer_id
+        (i + 1): customer_id
         for customer_id, i in customer_id2index.items()
     }
     article_index2id = {
-        i: article_id
+        (i + 1): article_id
         for article_id, i in article_id2index.items()
     }
 
