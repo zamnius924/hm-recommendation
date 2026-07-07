40866962c9c65d82d121241b11cb948bea000f07
Alexey Zamnius <a.zamnius@me.com>
2026-07-07 14:21:32 +0300

    add tower initialization to two-tower.py

--- two-tower.py
+++ two-tower.py
@@ -10,6 +10,7 @@ from scripts.generate_tt_data import generate_tt_data
 from scripts.load_db import load_db
 from scripts.paths import DATA_PROCESSED_DIR, MODELS_CONFIG_DIR
 from scripts.class_towers import Tower, TwoTower
+from scripts.class_tt_data import TwoTowerDataset
 
 # %% Загрузка параметров
 # Временное разделение на train-, valid- и test-выборки
@@ -32,7 +33,42 @@ con.close()
 # Создание мэппинга
 mapping = build_mapping(df_customer, df_article, df_pairs)
 
+# Вспомогательные данные
+df_aux = generate_dataset(df_customer, df_article, df_pairs, mapping)
+
 # Данные для Two-tower model
-tt_data = generate_dataset(df_customer, df_article, df_pairs, mapping)
+tt_dataset = TwoTowerDataset(df_aux)
 
 # %%
+customer_tower = Tower(
+    num_input_dim=tt_dataset.customer_num_dim,
+    num_hidden_dim=64,
+    cat_sizes=tt_dataset.customer_cat_sizes,
+    emb_dims=[8, 8]
+)
+
+article_tower = Tower(
+    num_input_dim=tt_dataset.article_num_dim,
+    num_hidden_dim=64,
+    cat_sizes=tt_dataset.article_cat_sizes,
+    emb_dims=[]
+)
+
+# %%
+batch_size = 10
+
+x_num = tt_dataset[0:batch_size]['article_num']
+x_cat = tt_dataset[0:batch_size]['article_cat']
+
+# %%
+#customer_tower(x_num, x_cat)
+article_tower(x_num, x_cat)
+
+# %%
+#train_loader = DataLoader(
+#    train_dataset,
+#    batch_size=1024,
+#    shuffle=True,
+#    drop_last=True,  # stable batch shapes
+#    num_workers=0,   # use 0 for notebook simplicity; increase for speed
+#)
