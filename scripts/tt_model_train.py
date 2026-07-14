89845c965cf7b1389c5530fb770526a308f7577d
Alexey Zamnius <a.zamnius@me.com>
2026-07-14 19:22:19 +0300

    add two-tower model training script

--- scripts/tt_model_train.py
+++ scripts/tt_model_train.py
@@ -1,52 +1,52 @@
 import torch.nn as nn
 
+from IPython.display import clear_output
 from scripts.class_towers import TwoTower
+from scripts.tt_model_inference import tt_model_inference
+from scripts.tt_model_plot_stats import tt_model_plot_stats
+from scripts.tt_model_training_step import tt_model_training_step
 from torch import optim
 from torch.utils.data import DataLoader
-from tqdm import tqdm
 
 def tt_model_train(
         tt_model: TwoTower,
-        data_loader: DataLoader,
+        data_loader_train: DataLoader,
+        data_loader_test: DataLoader,
         optimizer: optim.Optimizer,
-        criterion: nn.Module
-    ) -> float:
+        criterion: nn.Module,
+        num_epochs: int
+    ):
 
-    # Перевод модели в режим обучения
-    tt_model.train()
+    # История обучения (лоссы на обучающей и тестовой выборках)
+    train_loss_history, test_loss_history = [], []
 
-    # Потери на эпохе
-    total_loss = 0
+    # Обучение модели
+    for epoch in range(num_epochs):
 
-    for batch in tqdm(data_loader, desc='Train'):
-
-        # Обнуление градиента
-        optimizer.zero_grad()
-
-        # Forward pass – эмбеддинги покупателей и товаров
-        u, v = tt_model(
-            batch['customer_num'], 
-            batch['customer_cat'], 
-            batch['article_num'], 
-            batch['article_cat']
+        # Обучение
+        train_loss = tt_model_training_step(
+            tt_model,
+            data_loader_train,
+            optimizer,
+            criterion
         )
 
-        # Матрица скалярных произведений
-        logits = tt_model.similarity(u, v)
+        # Инференс
+        test_loss = tt_model_inference(
+            tt_model,
+            data_loader_test,
+            criterion
+        )
 
-        # Значение функционала потерь
-        loss = criterion(logits)
-        total_loss += loss.item()
+        # Добавление результатов
+        train_loss_history.append(train_loss)
+        test_loss_history.append(test_loss)
 
-        # Backward pass – оценка производных и градиента функции потерь
-        loss.backward()
+        clear_output()
 
-        # Шаг градиентного спуска – новые значения параметров
-        optimizer.step()
-
-    # Усреднение потерь по батчам
-    total_loss /= len(data_loader)
-
-    print(f'Loss on epoch (training): {total_loss}')
-
-    return total_loss
+        # Визуализация
+        tt_model_plot_stats(
+            train_loss_history,
+            test_loss_history,
+            'Two-Tower model'
+        )
