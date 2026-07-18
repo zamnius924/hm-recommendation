253de1876c37ecd05500f329d9d4a89df9e86bc4
Alexey Zamnius <a.zamnius@me.com>
2026-07-18 15:21:24 +0300

    add type annotations

--- scripts/build_mapping.py
+++ scripts/build_mapping.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def build_mapping(*dfs):
+def build_mapping(*dfs) -> dict:
 
     # Все товары и покупатели
     customer_series = []
--- scripts/generate_tt_inference.py
+++ scripts/generate_tt_inference.py
@@ -4,7 +4,9 @@ from scripts.class_tt_data import CustomerDataset, ArticleDataset
 from scripts.generate_tt_dfs import generate_tt_dfs
 from scripts.load_db import load_db
 
-def generate_tt_inference(split_dates: dict):
+def generate_tt_inference(
+        split_dates: dict
+    ) -> tuple[CustomerDataset, ArticleDataset, dict]:
 
     # --------------- Создание датафреймов с фичами и индексами пар -------------- #
     # Подключение к БД
