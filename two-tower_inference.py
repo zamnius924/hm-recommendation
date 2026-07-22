87278c6d4ca38b022934e6551ceba2823c36b91d
Alexey Zamnius <a.zamnius@me.com>
2026-07-22 17:12:01 +0300

    generate features for LTR

--- two-tower_inference.py
+++ two-tower_inference.py
@@ -2,18 +2,20 @@
 import json
 import torch
 
+from scripts.data.load_db import load_db
+from scripts.data.save_db import save_db
+from scripts.features.generate_features import generate_features
 from scripts.two_tower.generate_loader import generate_loader
+from scripts.two_tower.generate_tt_candidates import generate_tt_candidates
 from scripts.two_tower.generate_tt_datasets import generate_tt_inference
-#from scripts.two_tower.tt_model_candidates import tt_model_candidates
-from scripts.two_tower.generate_data import generate_tt_candidates
 from scripts.utils.paths import DATA_PROCESSED_DIR, MODELS_DIR
 
-# %% Загрузка
+# %% Импорт
 # Временное разделение на train-, valid- и test-выборки
 with open(file=DATA_PROCESSED_DIR / 'split_dates.json', mode='r') as file:
     dates = json.load(file)
 
-# Two-tower
+# Загрузка модели Two-tower
 tt_model = torch.load(
     f=MODELS_DIR / 'tt_model.pt',
     weights_only=False
@@ -26,7 +28,7 @@ customer_dataset, article_dataset, mapping = generate_tt_inference(dates['train'
 data_loader_customer = generate_loader(customer_dataset, mode='eval')
 data_loader_article = generate_loader(article_dataset, mode='eval')
 
-# %% Генерация кандидатов
+# %% Генерация кандидатов на основе Two-tower
 candidates_df = generate_tt_candidates(
     tt_model,
     data_loader_customer,
@@ -34,3 +36,19 @@ candidates_df = generate_tt_candidates(
     mapping,
     k=100
 )
+
+# %% Генерация фичей
+# Инициализация БД
+con = load_db()
+
+# Генерация фичей
+con = generate_features(con, 
+                        dates['train'], 
+                        candidates_df, 
+                        return_con=True)
+
+# %% Сохранение таблицы
+save_db(con, 'tt_df_train.parquet')
+
+# %% Отключение от БД
+con.close()
